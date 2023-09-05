1406ee7c6bf52a07ac6bf8d8cdfe8ffdf22401fa
Amar Singh <68085422+asinghh1@users.noreply.github.com>
2023-09-05 11:59:26 -0700

    Testing

--- app/src/androidTest/java/com/example/messagingapp/Test1.kt
+++ app/src/androidTest/java/com/example/messagingapp/Test1.kt
@@ -1,11 +1,14 @@
 package com.example.messagingapp
 
+import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.printToLog
 import org.junit.Rule
 import org.junit.Test
+
 
 class Test1 {
     @get:Rule
@@ -18,8 +21,14 @@ class Test1 {
         }
 
         rule.onNodeWithTag("First").printToLog("Rahul")
+        rule.onNodeWithTag("First").assertHasClickAction()
         rule.onNodeWithTag("First").performClick()
         rule.onNodeWithTag("hello").printToLog("Rahul")
+        rule.onNodeWithText("how are you")
+        rule.onNodeWithText("sjfwo").assertExists()
+        rule.onNodeWithText("Type your message").assertExists()
+        rule.onNodeWithText("Type your message").assertHasClickAction()
+
 
     }
 }
